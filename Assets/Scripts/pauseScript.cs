d801c3c4a7c388e72b7413c7b0be96a1bfd5eb5c
selam-ben-samet <basabdussamed@gmail.com>
2024-10-10 15:09:43 +0300

    ADD pause menu excludes music/sfx buttons

---
+++ Assets/Scripts/pauseScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class pauseScript : MonoBehaviour
+{
+    public Image pauseButton;
+
+
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+    public void pauseTheGame()
+    {
+        Time.timeScale = 0;
+        gameObject.SetActive(true);
+    }
+    public void resumeTheGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+    public void quitTheGame()
+    {
+        SceneManager.LoadScene("start");
+    }
+}
