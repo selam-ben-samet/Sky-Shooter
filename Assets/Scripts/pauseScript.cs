779235b542a948fda3d0105130f5176f92ae3deb
selam-ben-samet <basabdussamed@gmail.com>
2024-10-14 16:13:25 +0300

    ADD musics

--- Assets/Scripts/pauseScript.cs
+++ Assets/Scripts/pauseScript.cs
@@ -7,20 +7,27 @@ using UnityEngine.UI;
 
 public class pauseScript : MonoBehaviour
 {
-
+    private bool isGamePaused = false;
     void Start()
     {
         gameObject.SetActive(false);
     }
     public void pauseTheGame()
     {
-        Time.timeScale = 0;
-        gameObject.SetActive(true);
+        if (!isGamePaused)
+        {
+            Time.timeScale = 0;
+            gameObject.SetActive(true);
+            isGamePaused = true;
+        }
+        else { resumeTheGame(); }
+
     }
     public void resumeTheGame()
     {
         Time.timeScale = 1;
         gameObject.SetActive(false);
+        isGamePaused = false;
     }
     public void quitTheGame()
     {
